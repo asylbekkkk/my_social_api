70d62d46f2e826f2d3654375c95cc8f2ac9e0c6a
Asylbek <asylbekutegen669@email.com>
2026-02-28 12:09:52 +0500

    Fixed NameError: added import os in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,7 +140,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 2,
 }
 
 SIMPLE_JWT = {
@@ -151,3 +151,5 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 AUTH_USER_MODEL = 'api.User'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
